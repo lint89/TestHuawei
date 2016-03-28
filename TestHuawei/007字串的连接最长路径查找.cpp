1c6f1cd1a6175b8f1b600f6ad931a575b0c95a4d
lint89 <linsir345@gmail.com>
2016-03-28 22:30:40 +0800

    03282016

--- TestHuawei/007字串的连接最长路径查找.cpp
+++ TestHuawei/007字串的连接最长路径查找.cpp
@@ -1,43 +1,56 @@
 ﻿/*********************************************************
  * 题目描述
 
- * 描述：
- * 输入一个整数，将这个整数以字符串的形式逆序输出
- * 程序不考虑负数的情况，若数字含有0，则逆序形式也含有0，如输入为100，则输出为001
- *  
-
+ * 给定n个字符串，请对n个字符串按照字典序排列。 
  * 输入描述:
- * 输入一个int整数
+ * 输入第一行为一个正整数n(1≤n≤1000),下面n行为n个字符串(字符串长度≤100),字符串中只含有大小写字母。
 
 
  * 输出描述:
- * 将这个整数以字符串的形式逆序输出
+ * 数据输出n行，输出结果为按照字典序排列的字符串。
 
  * 输入例子:
- * 1516000
+ * 9
+ * cap
+ * to
+ * cat
+ * card
+ * two
+ * too
+ * up
+ * boat
+ * boot
 
  * 输出例子:
- * 0006151
+ * boat
+ * boot
+ * cap
+ * card
+ * cat
+ * to
+ * too
+ * two
+ * up
  *********************************************************/
 
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 
 int main()
 {
-	long num;
-	long ret;
-
+	int num;
 	cin >> num;
 
-	while (num > 0)
-	{
-		ret = num % 10;
-		cout << ret;
-		num /= 10;
+	string str;
+	vector<string> strVec;
+	while (num --) {
+		cin >> str;
+		strVec.push_back(str);
 	}
 
-	return 0;
+
 }
---
+++ TestHuawei/008字符个数统计.cpp
@@ -0,0 +1,52 @@
+﻿/*********************************************************
+ * 题目描述
+
+ * 编写一个函数，计算字符串中含有的不同字符的个数。字符在ACSII码范围内(0~127)。不在范围内的不作统计。
+
+ * 输入描述:
+ * 输入N个字符，字符在ACSII码范围内(0~127)。
+
+
+ * 输出描述:
+ * 输出字符的个数。
+
+ * 输入例子:
+ * abc
+
+ * 输出例子:
+ * 3
+ *********************************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+	string str;
+	getline(cin, str);
+	int bucket[128] = { 0 };
+
+	string::iterator begin = str.begin();
+	string::iterator end = str.end();
+	string::iterator pos;
+	for (pos = begin; pos != end; ++pos)
+	{
+		bucket[*pos] = +1;
+	}
+
+	int ret = 0;
+	for (int i = 0; i < 128; ++i)
+	{
+		if (bucket[i] > 0)
+			++ret;
+	}
+
+	cout << ret;
+
+	return 0;
+
+}
