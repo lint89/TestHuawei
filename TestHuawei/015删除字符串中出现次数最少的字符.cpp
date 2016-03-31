87e2b9980b2c2ef1f6a32a52c16fafb4af0470b0
lint89 <linsir345@gmail.com>
2016-04-01 00:11:38 +0800

    change to win

---
+++ TestHuawei/015删除字符串中出现次数最少的字符.cpp
@@ -0,0 +1,77 @@
+﻿/*********************************************************
+ * 题目描述
+ * 
+ * 实现删除字符串中出现次数最少的字符，若多个字符出现次数一样，则都删除。输出删除这些单词后的字符串，字符串中其它字符保持原来的顺序。
+ * 
+ * 输入描述:
+ * 字符串只包含小写英文字母, 不考虑非法输入，输入的字符串长度小于等于20个字节。
+ * 
+ * 
+ * 输出描述:
+ *     删除字符串中出现次数最少的字符后的字符串。
+ * 
+ * 输入例子:
+ *     abcdd
+ * 
+ * 输出例子:
+ *     dd
+ *********************************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    string str;
+ 
+    while (cin >> str) {
+
+        int num[26] = { 0 };
+
+        for (int i = 0; i < str.size(); ++i) {
+            num[str[i] - 'a']++;
+        }
+
+        int min = 20;   // 输入字符小于等于20字节
+        for (int i = 0; i < 26; ++i) {
+            if (num[i] == 0) continue;
+            if (num[i] < min) min = num[i];
+        }
+
+        string out;
+#if 0   // 记录删除字母，若字母等于删除字母，则标记
+        vector<char> del;
+
+        for (int i = 0; i < 26; ++i) {
+            if (num[i] == min)
+                del.push_back(i + 'a');
+        }
+
+        bool mark = true;
+        for (int i = 0; i < str.size(); ++i) {
+            mark = true;
+            for (int j = 0; j < del.size(); ++j) {
+                if (str[i] == del[j]) { 
+                    mark = false;
+                    break;
+                }
+            }
+            if (mark) out.push_back(str[i]);
+        }
+        del.clear();
+#endif        
+
+#if 1   // 若当前字母数大于最小个数，则输出
+        for (int i = 0; i < str.size(); ++i) {
+            if (num[str[i] - 'a'] > min)
+                out.push_back(str[i]);
+        }
+#endif        
+        cout << out << endl;
+        str.clear();
+        out.clear();
+    }
+}
---
+++ TestHuawei/016合唱队.cpp
@@ -0,0 +1,56 @@
+﻿/*********************************************************
+ * 题目描述
+ * 
+ * 计算最少出列多少位同学，使得剩下的同学排成合唱队形
+ * 说明：
+ * N位同学站成一排，音乐老师要请其中的(N-K)位同学出列，使得剩下的K位同学排成合唱队形。 
+ * 合唱队形是指这样的一种队形：设K位同学从左到右依次编号为1，2…，K，他们的身高分别为T1，T2，…，TK，   则他们的身高满足存在i（1<=i<=K）使得Ti<T2<......<Ti-1<Ti>Ti+1>......>TK。 
+ *      你的任务是，已知所有N位同学的身高，计算最少需要几位同学出列，可以使得剩下的同学排成合唱队形。 
+ *       
+ *********************************************************/
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int func(vector<int> &stu, int index, int len)
+{
+    int ret = 0;
+    int max, min;
+    for (int i = 0; i < index; ++i) {
+        max = stu[i];
+        if (stu[i + 1] < max)
+            ++ret;
+    }
+
+    for (int i = index; i < len - 1; ++i) {
+        min = stu[index];
+        if (stu[i + 1] > min)
+            ++ret;
+    }
+
+    return ret;
+}
+
+int main()
+{
+    int num;
+    vector<int> stu;
+    cin >> num;
+    for (int i = 0; i < num; ++i) {
+        int height = 0;
+        cin >> height;
+        stu.push_back(height);
+    }
+
+    int min = num;
+    int temp;
+    for (int i = 0; i < stu.size(); ++i) {
+       temp =  func(stu, i, stu.size());
+       if (temp < min) min = temp;
+    }
+
+    cout << min << endl;
+}
