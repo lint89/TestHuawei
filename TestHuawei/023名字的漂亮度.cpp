822221ac329351c3779a80f305aa5b1d51fb6364
lint89 <linsir345@gmail.com>
2016-04-06 08:53:49 +0800

    lint

---
+++ TestHuawei/023名字的漂亮度.cpp
@@ -0,0 +1,83 @@
+﻿/*********************************************************
+ * 题目描述
+
+ * 给出一个名字，该名字有26个字符串组成，定义这个字符串的“漂亮度”是其所有字母“漂亮度”的总和。
+ * 每个字母都有一个“漂亮度”，范围在1到26之间。没有任何两个字母拥有相同的“漂亮度”。字母忽略大小写。
+ * 给出多个名字，计算每个名字最大可能的“漂亮度”。
+
+ * 输入描述:
+ * 整数N，后续N个名字
+
+
+ * 输出描述:
+ * 每个名称可能的最大漂亮程度
+
+ * 输入例子:
+ * 2
+ * zhangsan
+ * lisi
+
+ * 输出例子:
+ * 192
+ * 101
+ *********************************************************/
+
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+void sort(int *val, int len)
+{
+	for (int i = 0; i < len; ++i)
+	{
+		for (int j = 0; j < len - i - 1; ++j)
+		{
+			int temp;
+			if (val[j] < val[j+1]) {
+				temp = val[j];
+				val[j] = val[j+1];
+				val[j+1] = temp;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	string str;
+
+	while (scanf("%d", &n) != EOF) {
+		while (n--) {
+			cin >> str;
+
+			for (int i = 0; i < str.size(); ++i)
+			{
+				if (str[i] >= 'A' && str[i] <= 'Z')
+					str[i] = str[i] + 'a' - 'A';
+			}
+
+			int num[26] = {0};
+
+			for (int i = 0; i < str.size(); ++i)
+				++num[str[i] - 'a'];
+
+			sort(num, 26);
+
+			int sum = 0;
+			int val = 26;
+			for (int i = 0; i < 26; ++i)
+			{
+				if (num[i] == 0) break;
+
+				sum += val * num[i];
+				--val;
+			}
+
+			cout << sum << endl;
+		}
+	}
+}
---
+++ TestHuawei/024字符串加解密.cpp
@@ -0,0 +1,126 @@
+﻿/*********************************************************
+ * 题目描述
+
+ * 1、对输入的字符串进行加解密，并输出。
+ * 2加密方法为：
+ * 当内容是英文字母时则用该英文字母的后一个字母替换，同时字母变换大小写,如字母a时则替换为B；字母Z时则替换为a；
+ * 当内容是数字时则把该数字加1，如0替换1，1替换2，9替换0；
+ * 其他字符不做变化。
+ * 3、解密方法为加密的逆过程。
+ *  
+ * 接口描述：
+ *     实现接口，每个接口实现1个基本操作：
+ * void Encrypt (char aucPassword[], char aucResult[])：在该函数中实现字符串加密并输出
+ * 说明：
+ * 1、字符串以\0结尾。
+ * 2、字符串最长100个字符。
+ *  
+ * int unEncrypt (char result[], char password[])：在该函数中实现字符串解密并输出
+ * 说明：
+ * 1、字符串以\0结尾。
+ *     2、字符串最长100个字符。
+ *  
+ *  
+ *  
+
+ * 输入描述:
+ * 输入说明
+ * 输入一串要加密的密码
+ * 输入一串加过密的密码
+
+
+ * 输出描述:
+ * 输出说明
+ * 输出加密后的字符
+ * 输出解密后的字符
+
+ * 输入例子:
+ * abcdefg
+ * BCDEFGH
+
+ * 输出例子:
+ * BCDEFGH
+ * abcdefg
+ *********************************************************/
+
+#include <cstdio>
+
+using namespace std;
+
+void Encrypt (char aucPassword[], char aucResult[])
+{
+	char ret;
+	int i = 0;
+	for (; aucPassword[i] != '\0'; ++i)
+	{
+		// 切换小写字母
+		if (aucPassword[i] == 'z')
+			ret = 'A';
+		if (aucPassword[i] >= 'a' && aucPassword[i] < 'z')
+			ret = aucPassword[i] - ('a' - 'A') + 1;
+
+		// 切换大写字母
+		if (aucPassword[i] == 'Z')
+			ret = 'a';
+		if (aucPassword[i] >= 'A' && aucPassword[i] < 'Z')
+			ret = aucPassword[i] + ('a' - 'A') + 1;
+
+		// 切换数字
+		if (aucPassword[i] == '9')
+			ret = '0';
+		if (aucPassword[i] >= '0' && aucPassword[i] < '9')
+			ret = aucPassword[i] + 1;
+
+		aucResult[i] = ret;
+	}
+	aucResult[i] = '\0';
+}
+
+int unEncrypt (char result[], char password[])
+{
+	char ret;
+	int i = 0;
+	for (; password[i] != '\0'; ++i)
+	{
+		// 切换小写字母
+		if (password[i] == 'a')
+			ret = 'Z';
+		if (password[i] > 'a' && password[i] <= 'z')
+			ret = password[i] - ('a' - 'A') - 1;
+
+		// 切换大写字母
+		if (password[i] == 'A')
+			ret = 'z';
+		if (password[i] > 'A' && password[i] <= 'Z')
+			ret = password[i] + ('a' - 'A') - 1;
+
+		// 切换数字
+		if (password[i] == '0')
+			ret = '9';
+		if (password[i] > '0' && password[i] <= '9')
+			ret = password[i] - 1;
+
+		result[i] = ret;
+	}
+	result[i] = '\0';
+	return 0;
+}
+
+int main()
+{
+	char aucPassword[100];
+	char aucResult[100];
+
+	char password[100];
+	char result[100];
+
+	while (scanf("%s", aucPassword) != EOF) {
+		Encrypt(aucPassword, aucResult);
+		printf("%s\n", aucResult);
+
+		scanf("%s", password);
+		unEncrypt(result, password);
+		printf("%s\n", result);
+	}
+
+}
